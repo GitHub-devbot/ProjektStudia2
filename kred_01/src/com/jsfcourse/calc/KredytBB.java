82b8a6c274b9aa7100b80e51e392ec0dc465118f
Szybki <Szybki@Szybki-Komputer>
2020-10-20 09:53:42 +0200

    first commit

---
+++ kred_01/src/com/jsfcourse/calc/KredytBB.java
@@ -0,0 +1,81 @@
+package com.jsfcourse.calc;
+
+import javax.inject.Inject;
+import javax.inject.Named;
+import javax.enterprise.context.RequestScoped;
+import javax.enterprise.context.SessionScoped;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+
+@Named
+@RequestScoped
+//@SessionScoped
+public class KredytBB {
+	private String kwota;
+	private String lata;
+	private String procent;
+	private Double wynik;	
+
+	@Inject
+	FacesContext ctx;
+	
+	public String getKwota() {
+		return kwota;
+	}
+
+	public void setKwota(String kwota) {
+		this.kwota = kwota;
+	}
+
+	public String getLata() {
+		return lata;
+	}
+
+	public void setLata(String lata) {
+		this.lata = lata;
+	}
+
+	public String getProcent() {
+		return procent;
+	}
+
+	public void setProcent(String procent) {
+		this.procent = procent;
+	}
+	
+	public Double getWynik() {
+		return wynik;
+	}
+
+	public void setWynik(Double wynik) {
+		this.wynik = wynik;
+	}
+
+	public boolean doTheMath() {
+		try {
+			double kwota = Double.parseDouble(this.kwota);
+			double lata = Double.parseDouble(this.lata);
+			double procent = Double.parseDouble(this.procent);
+			
+			wynik = (kwota * procent) * lata;
+
+			ctx.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Operacja wykonana poprawnie", null));
+			return true;
+		} catch (Exception e) {
+			ctx.addMessage(null,
+					new FacesMessage(FacesMessage.SEVERITY_ERROR, "B³¹d podczas przetwarzania parametrów", null));
+			return false;
+		}
+	}
+	
+	public String calc() {
+		if (doTheMath()) {
+			return "showresult";
+		}		
+		return null;
+	}
+	
+	public String info() {
+		return "info";
+	}
+}
